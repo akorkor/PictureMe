245d54b552d945999aaa39980135bf36a88eed0d
XiXi Luo <xixiluo@XiXis-MacBook-Pro.local>
2017-03-09 17:38:12 -0500

    devise login added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: "photos#home"
 
-  resources :user do
-    resources :photo
+  resources :photo do
+    resources :comment
   end
   resources :tag
 
-
-  get"/" => "photos#home"
+  # get"/" => "photos#home"
 
 end
