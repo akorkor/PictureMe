9843f4cf6c5abff5f94a0ae20df39bf75911e208
Alain Korkor <bebekorkor@msn.com>
2017-03-09 17:01:55 -0500

    modifications

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
 
   has_attached_file :image, :styles => lambda { |attachment| { :thumb =>
   		(attachment.instance.title == 'Special' ? "100x100#" : "64x64#") } }
-      
+
   validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 
 end
